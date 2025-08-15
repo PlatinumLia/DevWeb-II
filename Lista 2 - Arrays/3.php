654255a8800217c56fab227522ac2159d116b4e1
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2025-08-15 14:54:20 -0300

    Criar 3.php

---
+++ Lista 2 - Arrays/3.php
@@ -0,0 +1,20 @@
+<?php
+//declaração das variáveis contendo os arrays
+$flores = array("Orquídea", "Margarida", "Petúnia");
+$frutas = array("Laranja", "Maçã", "Limão");
+$cidades = array("Foz do Iguaçu", "Cascavel", "Toledo");
+$locaisTuristicos = array("Itaipu", "Cataratas", "Parque das aves");
+$listaTudo = array($flores, $frutas, $cidades, $locaisTuristicos);
+
+echo "<table border = 1>";
+echo "<tr>";
+echo "<th>" . "Flores" . "</th>";
+echo "<th>" . "Frutas" . "</th>";
+echo "<th>" . "Cidades" . "</th>";
+echo "<th>" . "Locais Turísticos" . "</th>";
+echo "</tr>";
+foreach(){
+    
+}
+
+?>
