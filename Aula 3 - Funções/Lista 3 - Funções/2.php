ebe4039f24a8c444b9b5acd282bdac487b4a25fa
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2025-08-18 20:56:38 -0300

    Add files via upload

--- Aula 3 - Funções/Lista 3 - Funções/2.php
+++ Aula 3 - Funções/Lista 3 - Funções/2.php
@@ -10,6 +10,12 @@ function calculaPerimetroRetangulo($lado1, $lado2, $lado3, $lado4){
     return $resultado;
 }    
 
-echo "Área e Perímetro de 3 retângulo: " . "<br><br>"
+//mostra os resultados na página HTML
+echo "Área e Perímetro de 3 retângulo: " . "<br><br>";
+echo "Retângulo 1: <br>" . "Área: " . areaRetangulo(2, 2) . "<br>Perímetro: " . calculaPerimetroRetangulo(2, 2, 2, 2) . "<br><br>";
+
+echo "Retângulo 2: <br>" . "Área: " . areaRetangulo(10, 4) . "<br>Perímetro: " . calculaPerimetroRetangulo(10, 10, 4, 4) . "<br><br>";
+
+echo "Retângulo 3: <br>" . "Área: " . areaRetangulo(50, 25) . "<br>Perímetro: " . calculaPerimetroRetangulo(50, 50, 25, 25);
 
 ?>
