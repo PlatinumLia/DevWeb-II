51becb42b3e6b8e26b0aa1ab6807e79a83d5509b
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2025-08-20 20:03:25 -0300

    Add files via upload

---
+++ Aula 5 - Formulários/Exercicíos_slide/atv2/cor_form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cor</title>
+</head>
+<body>
+    <form method="post" action="cor_exec.php">
+        <select name="cor">
+            <option value="">---Selecione uma cor---</option value="">
+            <option value="tomato" >Tomato</option>
+            <option value="dodgerblue">DodgerBlue</option>
+            <option value="gray">Gray</option>
+            <option value="violet">Violet</option>
+            <option value="orange">Orange</option>
+            <option value="mediumseagreen">MediumSeaGreen</option>
+            <option value="stateblue">StateBlue</option>
+            <option value="lightgray">LightGray</option>
+        </select>
+        <br><br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
