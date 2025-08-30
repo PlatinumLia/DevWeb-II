7a592cc978b892d99a5429004e262d5c4379e1f7
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2025-08-29 21:48:59 -0300

    Add files via upload

--- Aula 6 - CadastroPersistente/Livros/livros.php
+++ Aula 6 - CadastroPersistente/Livros/livros.php
@@ -9,23 +9,59 @@ include_once("persistencia.php");
 $livros = buscarDados("livros.json");
 //print_r($livros);
 
+$titulo = "";
+$genero = "";
+$paginas = "";
+$autor = ""; 
+$mensagemErro = "";
+
 //verifica se os valores foram enviados
 if(isset($_POST["titulo"])){
-    $titulo = $_POST["titulo"];
-    $genero = $_POST["genero"];
+    //já clicou em "Enviar"
+    $titulo = trim($_POST["titulo"]);
+    $genero = trim($_POST["genero"]);
     $paginas = $_POST["paginas"];
+    $autor = trim($_POST["autor"]);
 
-    $livro = array(
-        "id" => uniqid(),
-        "titulo" => $titulo,
-        "genero" => $genero,
-        "paginas" => $paginas
-    );
+    //validar os dados
+    $erros = array();
+    if($titulo == ''){
+        array_push($erros, "Informe o título");
+    }else if(strlen($titulo)<=3){
+        array_push($erros, "Informe um título com mais de 3 caracteres");
+    }
 
-    array_push($livros, $livro);
-    salvarDados($livros, "livros.json");
+    if($genero == ''){
+        array_push($erros, "Informe o gênero do livro");
+    }
 
-    header("location: livros.php"); //forçar o recarregamento da página para evitar o reenvio do formulário
+    if($paginas == ''){
+        array_push($erros, "Informe o número de páginas");
+    }else if($paginas <= 0){
+        array_push($erros, "O número de páginas precisa ser maior que 0");
+    }
+    
+    if($autor == ''){
+        array_push($erros, "Informe o autor");
+    }
+
+    if(empty($erros)){
+        //passou as validações, salva no arquivo
+        $livro = array(
+            "id" => uniqid(),
+            "titulo" => $titulo,
+            "genero" => $genero,
+            "paginas" => $paginas,
+            "autor" => $autor
+        );
+    
+        array_push($livros, $livro);
+        salvarDados($livros, "livros.json");
+    
+        header("location: livros.php"); //forçar o recarregamento da página para evitar o reenvio do formulário
+    }else{
+        $mensagemErro = implode("<br>", $erros);
+    }
 }
 ?>
 
@@ -42,24 +78,33 @@ if(isset($_POST["titulo"])){
 <h1>Cadastro de livros</h1>
 
 <h3>Cadastre seu livro aqui</h3>
+<!--- <form method="post" onsubmit="return validar();"> --->
 <form method="post">
-    <input type="text" name="titulo" placeholder="Informe o título" />
+    <input type="text" name="titulo" id="titulo" placeholder="Informe o título" value="<?= $titulo?>"/>
     <br><br> 
 
-    <select name="genero">
+    <select name="genero" id="genero">
         <option value="">--Selecione o gênero--</option>
-        <option value="D">Drama</option>
-        <option value="F">Ficção</option>
-        <option value="R">Romance</option>
-        <option value="O">Outro</option>
+        <!-- xxx ? xxx : xxx => operador ternário-->
+        <option value="D" <?= $genero == 'D' ? 'selected' : '' ?> >Drama</option>
+        <option value="F" <?= $genero == 'F' ? 'selected' : '' ?> >Ficção</option>
+        <option value="R" <?= $genero == 'R' ? 'selected' : '' ?> >Romance</option>
+        <option value="O" <?= $genero == 'O' ? 'selected' : '' ?> >Outro</option>
     </select>
     <br><br>
 
-    <input type="number" name="paginas" placeholder="Informe o número de páginas">
+    <input type="number" name="paginas" id="numPaginas" placeholder="Informe o número de páginas" value="<?= $paginas?>"/>
+    <br><br>
+    
+    <input type="text" name="autor" id="autor" placeholder="Informe o autor" value="<?= $autor?>">
     <br><br>
 
     <input type="submit" value="Enviar" />
 </form>
+
+<div id="divErro" style="color: red;">
+    <?= $mensagemErro?>
+</div>
 
 <h3>Livros cadastrados</h3>
 
@@ -69,6 +114,7 @@ if(isset($_POST["titulo"])){
         <th>Título</th>
         <th>Gênero</th>
         <th>Quant. Páginas</th>
+        <th>Autor</th>
         <th>Excluir</th>
     </tr>
 
@@ -78,10 +124,13 @@ if(isset($_POST["titulo"])){
             <td><?= $l['titulo']?></td>
             <td><?= $l['genero']?></td>
             <td><?= $l['paginas']?></td>
+            <td><?= $l['autor']?></td>
             <td><a href="excluir.php?id=<? $l['id']?>" onclick="return confirm('Confirma a exclusão?')">Excluir</a></td>
         </tr>
     <?php endforeach;?>
 </table>
+
+<script src="validacao.js"></script>
 
 </body>
 </html>
