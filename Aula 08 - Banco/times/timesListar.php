8fc815924a64efa8e9d0a5232ccddaca1ef0ae98
Lia <134401063+PlatinumLia@users.noreply.github.com>
2025-09-26 21:56:02 -0300

    Add files via upload

---
+++ Aula 08 - Banco/times/timesListar.php
@@ -0,0 +1,26 @@
+<?php
+include_once("Connection.php");
+
+$conn = Connection::getConnection();
+print_r($conn); //mostra o que há na variável "$conn"
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Times</title>
+</head>
+<body>
+    <h1>Aula banco de dados - Times</h1>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Cidade</th>
+            <th></th>
+        </tr>
+    </table>
+</body>
+</html>
