686a209473538ae480435c3967bfb689d8dea104
Lia <134401063+PlatinumLia@users.noreply.github.com>
2025-11-04 21:31:44 -0300

    Add files via upload

---
+++ Aula 09\10\11\12\13 - Aplicação/mvc_alunos/view/alunos/editar.php
@@ -0,0 +1,58 @@
+<?php
+include_once(__DIR__ . "/../../controller/AlunoController.php");
+
+$alunoCont = new AlunoController();
+$msgErro = "";
+$aluno = NULL;
+
+if(isset($_POST['nome'])){ //2ª etapa:
+    //já clicou no gravar
+    //pegar os valores preenchidos no form
+    //dados receberão tratamento com operador ternário
+    //diferença do editar para o inserir: terá uma variável pegando o id
+    $id = $_POST['id'];
+    $nome = trim($_POST['nome']) ? trim($_POST['nome']) : NULL;
+    $idade = is_numeric($_POST['idade']) ? $_POST['idade'] : NULL;
+    $estrangeiro = trim($_POST['estrang']) ? trim($_POST['estrang']) : NULL;
+    $idCurso = is_numeric($_POST['curso']) ? $_POST['curso'] : NULL;
+
+    $aluno = new Aluno();
+    $aluno->setId($id);
+    $aluno->setNome($nome);
+    $aluno->setIdade($idade);
+    $aluno->setEstrangeiro($estrangeiro);
+
+    //verificando se há um id para criar um objeto curso
+    if($idCurso){ //caso haja -> cria um objeto Curso e setando seu atributo "id"
+        $curso = new Curso();
+        $curso->setId($idCurso);
+        $aluno->setCurso($curso);
+    }else{ //caso não haja -> seta como NULL
+        $aluno->setCurso(NULL);
+    }
+    //print_r($aluno); //verificando o que há no objeto $aluno
+    //variável para capturar os erros 
+    $erros = $alunoCont->editar($aluno); //chamando o controller para salvar a edição
+
+    if(!$erros){
+        header("location: listar.php"); //voltando para a página que lista os alunos
+    }else{
+        $msgErro = implode("<br>", $erros);
+    }
+}else{ //1ª etapa: 
+    //abriu o form para fazer a edição dos dados
+    $id = 0;
+    if(isset($_GET['id'])){ //verificando se há o id
+        $id = $_GET['id'];
+    }
+
+    $aluno = $alunoCont->buscarPorId($id);
+    if(!$aluno){ //tratamento
+        echo "Aluno não encontrado. <br>";
+        echo "<a href='listar.php'>Voltar</a>";
+        exit; //para a requisiçao
+    }
+}
+
+include_once(__DIR__ . "/form.php");
+?>
---
+++ Aula 09\10\11\12\13 - Aplicação/mvc_alunos/view/alunos/form.php
@@ -0,0 +1,85 @@
+<?php
+//carregar a lista de cursos
+include_once(__DIR__ . "/../../controller/CursoController.php");
+include_once(__DIR__ . "../../../util/config.php");
+include_once(__DIR__ . "/../include/menu.php");
+
+$cursoCont = new CursoController(); //objeto para carregar o "CursoController" 
+$cursos = $cursoCont->listar(); //chamando o método listar
+//print_r($cursos); //confirmando o que está gravado na variável "$cursos"
+
+include_once(__DIR__ . "/../include/header.php");
+?>
+
+<h3> <?= $aluno && $aluno->getId() > 0 ? 'Editar' : 'Inserir' ?> aluno </h3>
+
+<div class="row">
+    <div class="col-6">
+        <form method="POST" action="">
+
+            <div class="mb-3">
+                <label for="txtNome" class="form-label">Nome:</label>
+                <input type="text" class="form-control" id="txtNome" name="nome" placeholder="Informe o nome" 
+                    value="<?= $aluno ? $aluno->getNome() : "" ?>">
+            </div>
+
+            <div class="mb-3">
+                <label for="txtIdade" class="form-label">Idade:</label>
+                <input type="number" class="form-control" id="txtIdade" name="idade" placeholder="Informe a idade" 
+                    value="<?= $aluno ? $aluno->getIdade() : "" ?>">
+            </div>
+
+            <div class="mb-3">
+                <label for="selEstrang" class="form-label">Estrangeiro:</label>
+                <select name="estrang" class="form-select" id="selEstrang">
+                    <option value="">----Selecione----</option>
+                    <option value="S" 
+                        <?= $aluno && $aluno->getEstrangeiro() == 'S' ? 'selected' : '' ?> >Sim</option>
+                    <option value="N" 
+                        <?= $aluno && $aluno->getEstrangeiro() == 'N' ? 'selected' : '' ?> >Não</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="selCurso" class="form-label">Curso:</label>
+                <select name="curso" class="form-select" id="selCurso">
+                    <option value="">----Selecione----</option>
+                    <?php foreach($cursos as $c): ?>
+                        <option value="<?= $c->getId(); ?>" 
+                            <?php 
+                                if($aluno && $aluno->getCurso() && $aluno->getCurso()->getId() == $c->getId()){
+                                    echo "selected";
+                                }
+                            ?> 
+                        > <!-- fechamento da tag <option> -->
+                            <?= $c->getCursoTurno() ?>
+                        </option>    
+                    <?php endforeach; ?>
+                </select>
+            </div>
+
+            <input type="hidden" value="<?= $aluno ? $aluno->getId() : 0 ?>" name="id">
+
+            <div class="mt-2">
+                <button type="submit" class="btn btn-dark">Gravar</button>
+            </div>
+
+        </form>
+    </div> <!-- fecha a div col -->
+
+    <div class="col-6">
+        <?php if($msgErro): ?>
+            <div class="alert alert-dark mt-5">
+                <?= $msgErro?>
+            </div>
+        <?php endif; ?>
+    </div> <!-- fecha a 2ª div col -->    
+</div> <!-- fecha a div do row -->
+
+<div>
+    <a href="listar.php" class="btn btn-outline-dark mt-5">Voltar</a>
+</div>
+
+<?php
+include_once(__DIR__ . "/../include/footer.php");
+?>
